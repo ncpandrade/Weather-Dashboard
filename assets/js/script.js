72b6a3420deed4c4e6c1d4ee58a6c17a4574d1ac
Nadine <nadine_paez@hotmail.com>
2021-09-26 12:06:21 -0400

    api set up

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//get user City function
+var getUserCity = function(city) {
+    var apiUrl = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=0709fe582a3226805c5caaebd2b415a5
+
+}
