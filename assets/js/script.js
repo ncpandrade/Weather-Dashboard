4812fb9d537d7361bf737b60f869ed5b107e34d6
Nadine <nadine_paez@hotmail.com>
2021-09-26 12:39:28 -0400

    api request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 //get user City function
 var getUserCity = function(city) {
-    var apiUrl = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=0709fe582a3226805c5caaebd2b415a5
-
+    //format the weather api url
+    var apiUrl = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=0709fe582a3226805c5caaebd2b415a5'
+    
+    //create a variable called 'city' to be used in api Url
+    var city = document.querySelector('#cityname').value;
+   
+    //make a request to the url concatenating 'city' value
+    fetch(apiUrl)
+        .then(function(response) {
+             return response.json();
+    })
+        .then(function(response) {
+             console.log(response.data[0]);
+        });
 }
+
+    //     //using fetch's .ok functionality to check if a username exists
+    //     if (response.ok) {
+    //       response.json().then(function(data) {
+    //         console.log(data);
+    //       });
+    //     } else {
+    //       alert("Error: GitHub User Not Found");
+    //     }
+    //   })
+
+
+    //     }
+    
