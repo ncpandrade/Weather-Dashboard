321d9778408fa8e4fa0065828db5c1f2fd3808e7
Nadine <nadine_paez@hotmail.com>
2021-09-26 14:48:00 -0400

    display current weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,68 +5,65 @@ var getUserWeather = function (city) {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=0709fe582a3226805c5caaebd2b415a5'
 
     //make a request to the url concatenating 'city' value
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log(response);
-        });
-}
-//variables to store a reference to the <form> element using each id
-var userFormEl = document.querySelector("#user-form");
-var cityInputEl = document.querySelector("#cityname");
-//variables to reference DOM elements
-var currentWeatherContainer = document.querySelector('#current-weather-container');
-//Function to obtain city input on button click
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    // get value from input element
-    var city = cityInputEl.value.trim();
-    console.log(city);
-    //check if cityname was entered
-    if (city) {
-        //if yes, pass cityname into getUserWeather()
-        getUserWeather(city);
-        //clear value from the <input> element
-        cityInputEl.value = "";
-    } else {
-        alert("Please enter a City name");
-    }
-    console.log(event);
-};
-//clear old content from container
-currentWeatherContainer.textContent = "";
+    fetch(apiUrl).then(function (response) {
+            response.json().then(function (data) {
+                displayCurrentWeather(data, city);
+            });
+        
+      });
+   
+    // .then(function (response) {
+    //     return response.json();
+    // })
+    // .then(function (response) {
+    //     console.log(response);
+    // });
+    // }
+    //variables to store a reference to the <form> element using each id
+    var userFormEl = document.querySelector("#user-form");
+    var cityInputEl = document.querySelector("#cityname");
+    //variables to reference DOM elements
+    var currentWeatherContainer = document.querySelector('#current-weather-container');
+    //Function to obtain city input on button click
+    var formSubmitHandler = function (event) {
+        event.preventDefault();
+        // get value from input element
+        var city = cityInputEl.value.trim();
+        console.log(city);
+        //check if cityname was entered
+        if (city) {
+            //if yes, pass cityname into getUserWeather()
+            getUserWeather(city);
+            //clear value from the <input> element
+            cityInputEl.value = "";
+        } else {
+            alert("Please enter a City name");
+        }
+        console.log(event);
+    };
+    // clear old content from container
+    // currentWeatherContainer.textContent = "";
 
-//FUNCTION to display weather
-var displayCurrentWeather = function (weather) {
-    console.log(weather);
-    console.log(city);
+    //FUNCTION to display weather
+    var displayCurrentWeather = function (weather) {
+        console.log(weather);
+        console.log(city);
 
-    //format weather info
-    var cityInfo = weather[i].name;
+        //format weather info
+        var cityInfo = weather[i].name
 
-    //create conatiner for current weather
-    var currentCityEl = document.createElement("div");
-    currentWeatherContainer.classList = "list-item flex-row justify-space-between align-center";
+        //create conatiner for current weather
+        var currentWeatherEl = document.createElement("div");
+        currentWeatherContainer.classList = "list-item flex-row justify-space-between align-center";
 
+        //create container for current city and date
+        var currentCityEl = document.createElement("div");
+        currentCityEl.textContent = cityInfo;
 
-}
+        //append to container
+        currentWeatherContainer.appendChild(currentCityEl);
 
-// submit event listener - when user clicks Get City formSubmitHandler is activated
-userFormEl.addEventListener("submit", formSubmitHandler);
+        // submit event listener - when user clicks Get City formSubmitHandler is activated
+        userFormEl.addEventListener("submit", formSubmitHandler);
 
-
-        // //using fetch's .ok functionality to check if a username exists
-        // if (response.ok) {
-        //   response.json().then(function(data) {
-        //     console.log(data);
-        //   });
-        // } else {
-        //   alert("Error: GitHub User Not Found");
-        // }
-
-
-
-    //     }
-
+    }}
