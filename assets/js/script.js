304d61e60d03b33f7e921fbc63852270a882ce8c
Nadine <nadine_paez@hotmail.com>
2021-09-26 13:41:43 -0400

    display weather from api request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
 
 //get user City function
-var getUserWeather = function(city) {
+var getUserWeather = function (city) {
     //format the weather api url
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=0709fe582a3226805c5caaebd2b415a5'
-    
+
     //make a request to the url concatenating 'city' value
     fetch(apiUrl)
-        .then(function(response) {
-             return response.json();
-    })
-        .then(function(response) {
-             console.log(response);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response);
         });
 }
 //variables to store a reference to the <form> element using each id
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#cityname");
-
+//variables to reference DOM elements
+var currentWeatherContainer = document.querySelector('#current-weather-container');
 //Function to obtain city input on button click
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     // get value from input element
     var city = cityInputEl.value.trim();
@@ -29,25 +30,37 @@ var formSubmitHandler = function(event) {
         getUserWeather(city);
         //clear value from the <input> element
         cityInputEl.value = "";
-}   else {
+    } else {
         alert("Please enter a City name");
-}
+    }
     console.log(event);
 };
+
+//FUNCTION to display weather
+var displayCurrentWeather = function (weather) {
+    console.log(weather);
+    console.log(city);
+
+    //clear old content from container
+    currentWeatherContainer.textContent = "";
+
+    //
+
+}
 
 // submit event listener - when user clicks Get City formSubmitHandler is activated
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 
-    //     //using fetch's .ok functionality to check if a username exists
-    //     if (response.ok) {
-    //       response.json().then(function(data) {
-    //         console.log(data);
-    //       });
-    //     } else {
-    //       alert("Error: GitHub User Not Found");
-    //     }
-    //   })
+        // //using fetch's .ok functionality to check if a username exists
+        // if (response.ok) {
+        //   response.json().then(function(data) {
+        //     console.log(data);
+        //   });
+        // } else {
+        //   alert("Error: GitHub User Not Found");
+        // }
+
 
 
     //     }
