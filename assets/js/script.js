640225be705e7b21f0f8ed37e354121cf773fa94
Nadine <nadine_paez@hotmail.com>
2021-09-27 11:43:44 -0400

    displayForecast loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#cityname");
 //variables to reference DOM elements
 var currentWeatherContainer = document.querySelector('#weatherCurrent');
+var forecastContainer = document.querySelector('#forecast-container');
 
 
 //get user Weather function
@@ -15,32 +16,26 @@ var getUserWeather = function (city) {
         response.json().then(function (data) {
             displayCurrentWeather(data, city);
 
-            //return fetch request to the one call api with the coordinates
+            //get city coordinates from returend object
+            var coordLat = data.coord.lat;
+            var coordLon = data.coord.lon;
+            console.log(coordLat, coordLon);
+
+            //return fetch request to the one call api with the coordinates 
             return fetch(
-                'https://api.openweathermap.org/data/2.5/onecall?lat=' +coordLat + '&lon=' + coordLan + '&exclude=current, minutely,hourly, alerts&appid=0709fe582a3226805c5caaebd2b415a5'
+                'https://api.openweathermap.org/data/2.5/onecall?lat=' + coordLat + '&lon=' + coordLon + '&exclude=current, minutely,hourly, alerts&appid=0709fe582a3226805c5caaebd2b415a5'
             );
         })
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(reponse) {
-            console.log(response);
-        });
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                console.log(response);
+                displayForecast();
+            });
     });
 }
 
-//FUNCTION to make api request for forecast info
-var getForecast = function () {
-    //format forecast api
-    var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +coordLat + '&lon=' + coordLan + '&exclude=current, minutely,hourly, alerts&appid=0709fe582a3226805c5caaebd2b415a5';
-    
-    //make fetch request concatenating coordLat and coordLon
-    fetch(forecastApiUrl).then(function (response){
-        response.json().then(function () {
-            console.log(response);
-        })
-    })
-}
 
 //Function to obtain city input on button click
 var formSubmitHandler = function (event) {
@@ -57,7 +52,7 @@ var formSubmitHandler = function (event) {
     } else {
         alert("Please enter a City name");
     }
-    
+
     console.log(event);
 };
 // clear old content from container
@@ -75,11 +70,6 @@ var displayCurrentWeather = function (weather) {
     //format today's weather info
     var weatherInfo = weather.main.temp + weather.wind.speed + weather.main.humidity;
 
-    //get city coordinates
-    var coordLat = weather.coord.lat;
-    var coordLon = weather.coord.lon;
-    console.log(coordLat, coordLon);
-    
     //create container for current weather
     var currentWeatherEl = document.createElement("div");
     currentWeatherEl.classList = "list-item flex-row justify-space-between align-center";
@@ -98,10 +88,46 @@ var displayCurrentWeather = function (weather) {
     //append to currentWeatherContainer DOM
     currentWeatherContainer.appendChild(todaysWeatherInfoEl);
 }
- //FUNCTION to display forecast weather
- var displayForecast = function() {
+//FUNCTION to display forecast weather
+var displayForecast = function (weather) {
+    //clear old content
+    forecastContainer.textContent = "";
 
- }
+    //loop over forecast days max 5
+    for (var i = 0; i < 5; i++) {
+
+        //format forecast info temp,humidity,wind
+        var forecastInfo = weather.daily[i].temp.day;
+        console.log(forecastInfo);
+
+        // //create a container for each forecast day
+        // var forecastEl = document.createElement("div");
+        // forecastEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // //create a span element for date
+        // var dateEl = document.createElement("span");
+        // //dateEl.textContent = enter date here
+
+        // //append to container
+        // forecastEl.appendChild(dateEl);
+
+        // //create icon element
+        // var iconEl = document.createElement("span");
+        // iconEl.classList = "flex-row align-center";
+        // //insert icon
+
+        // //append to container
+        // forecastEl.appendchile(iconEl);
+
+        // //create div element for weather details
+        // var forecastInfoEl = document.createElement("div");
+        // // forecastInfoEl.textContent = 
+        // //
+
+
+
+    }
+}
 
 // submit event listener - when user clicks Get City formSubmitHandler is activated
 userFormEl.addEventListener("submit", formSubmitHandler);
