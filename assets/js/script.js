7dec339dae99d5c7ad32008f0875b3b4142599b0
Nadine <nadine_paez@hotmail.com>
2021-09-28 16:50:25 -0400

    current weather and forecast info displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var forecastContainer = document.querySelector('#forecast-container');
 //get user Weather function
 var getUserWeather = function (city) {
     //format the weather api url
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=0709fe582a3226805c5caaebd2b415a5'
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=0709fe582a3226805c5caaebd2b415a5'
 
     //make a request to the url concatenating 'city' value
     fetch(apiUrl).then(function (weatherResponse) {
@@ -23,7 +23,7 @@ var getUserWeather = function (city) {
 
             //return fetch request to the one call api with the coordinates 
             return fetch(
-                'https://api.openweathermap.org/data/2.5/onecall?lat=' + coordLat + '&lon=' + coordLon + '&exclude=current, minutely,hourly, alerts&appid=0709fe582a3226805c5caaebd2b415a5'
+                'https://api.openweathermap.org/data/2.5/onecall?lat=' + coordLat + '&lon=' + coordLon + '&units=imperial&exclude=current, minutely,hourly, alerts&appid=0709fe582a3226805c5caaebd2b415a5'
             );
         })
             .then(function (forecastResponse) {
@@ -58,17 +58,29 @@ var formSubmitHandler = function (event) {
 // clear old content from container
 currentWeatherContainer.textContent = "";
 
-//FUNCTION to display current weather
+//FUNCTION to display CURRENT WEATHER
 var displayCurrentWeather = function (weather) {
     console.log(weather);
-    // console.log(city);
+
+    //format forecastDateUnix to regular date
+    function convertTimestamp() {
+        var unixTimestamp = weather.dt;
+        dateObj = new Date(unixTimestamp * 1000);
+        utcString = dateObj.toUTCString();
+        currentDate = dateObj.toLocaleDateString();
+
+    }
+    convertTimestamp();
 
     //format weather info
-    var cityInfo = weather.name
+    var cityInfo = weather.name + '(' + currentDate + ')';
     console.log(cityInfo);
 
     //format today's weather info
-    var weatherInfo = weather.main.temp + weather.wind.speed + weather.main.humidity;
+    var tempInfo = weather.main.temp;
+    var windInfo = weather.wind.speed;
+    var humInfo = weather.main.humidity;
+    //UV info here
 
     //create container for current weather
     var currentWeatherEl = document.createElement("div");
@@ -83,48 +95,64 @@ var displayCurrentWeather = function (weather) {
 
     //create container for today's weather info
     var todaysWeatherInfoEl = document.createElement("div");
-    todaysWeatherInfoEl.textContent = weatherInfo;
+    todaysWeatherInfoEl.textContent = 'Temp: ' + tempInfo + 'Wind: ' + windInfo + 'Humidity: ' + humInfo;
 
     //append to currentWeatherContainer DOM
     currentWeatherContainer.appendChild(todaysWeatherInfoEl);
 }
 //FUNCTION to display forecast weather
 var displayForecast = function (forecastArr) {
+
     //clear old content
     forecastContainer.textContent = "";
 
+
+
     //loop over forecast days max 5
-    for (var i = 0; i < 5; i++) {
-        //create variable to hold temp,wind,humidity
+    for (var i = 1; i < 6; i++) {
+        //create variable to hold temp,wind,humidity, date
         var forecastTemp = "Temp: " + forecastArr.daily[i].temp.day;
-        var forecastWind = "Wind: " + forecastArr.daily[i].weather[0].wind_speed;
-        var forecastHum = "Humindity: " + forecastArr.daily[i].weather.feels_like.humidity;
-        
-        console.log(forecastTemp, forecastWind, forecastHum);
+        var forecastWind = "Wind: " + forecastArr.daily[i].wind_speed;
+        var forecastHum = "Humidity: " + forecastArr.daily[i].humidity;
+        //format forecastDateUnix to regular date
+        function convertTimestamp() {
+            var unixTimestamp = forecastArr.daily[i].dt;
+            dateObj = new Date(unixTimestamp * 1000);
+            utcString = dateObj.toUTCString();
+            forecastDate = dateObj.toLocaleDateString();
+
+        }
+        convertTimestamp();
+
+        console.log(forecastDate, forecastTemp, forecastWind, forecastHum);
 
         // //create a container for each forecast day
-        // var forecastEl = document.createElement("div");
-        // forecastEl.classList = "list-item flex-row justify-space-between align-center";
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "flex-row justify-space-between align-center";
 
         // //create a span element for date
-        // var dateEl = document.createElement("span");
-        // //dateEl.textContent = enter date here
+        var dateEl = document.createElement("span");
+        dateEl.textContent = forecastDate;
 
         // //append to container
-        // forecastEl.appendChild(dateEl);
+        forecastEl.appendChild(dateEl);
 
         // //create icon element
         // var iconEl = document.createElement("span");
-        // iconEl.classList = "flex-row align-center";
         // //insert icon
 
         // //append to container
         // forecastEl.appendchile(iconEl);
 
-        // //create div element for weather details
-        // var forecastInfoEl = document.createElement("div");
-        // // forecastInfoEl.textContent = 
-        // //
+        //create div element for weather details
+        var forecastInfoEl = document.createElement("div");
+        forecastInfoEl.textContent = forecastTemp + forecastWind + forecastHum;
+
+        //append forecastInfoEl to forecastEl container
+        forecastEl.appendChild(forecastInfoEl);
+
+        //append forecastEl to forecastContainer
+        forecastContainer.appendChild(forecastEl);
 
 
 
